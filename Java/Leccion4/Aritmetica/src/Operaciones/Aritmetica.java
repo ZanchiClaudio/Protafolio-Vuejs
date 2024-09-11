5b4ef3345a0ba550db3959d0e58db1a66502b222
Zanchi Claudio Esteban <estebanzanchi@gmail.com>
2024-09-10 23:07:03 -0300

    Clase 4 de java

---
+++ Java/Leccion4/Aritmetica/src/Operaciones/Aritmetica.java
@@ -0,0 +1,18 @@
+
+package Operaciones;
+
+
+public class Aritmetica {
+    //Atributos de la clase
+    int a; 
+    int b;
+    
+    //Metodos
+    public void sumarNumero(){
+        //cuerpo de la clase, las variables aqui son locales. 
+        int resultado = a + b;
+        System.out.println("resultado = " + resultado);
+        
+    }
+    
+}
