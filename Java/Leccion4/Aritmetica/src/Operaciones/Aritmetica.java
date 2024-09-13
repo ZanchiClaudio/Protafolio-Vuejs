95e1076a180149465902560503d01dc27c6c9c83
Zanchi Claudio Esteban <estebanzanchi@gmail.com>
2024-09-12 21:18:46 -0300

    Clase 5 de java, pasos de argumentos y oprador this

--- Java/Leccion4/Aritmetica/src/Operaciones/Aritmetica.java
+++ Java/Leccion4/Aritmetica/src/Operaciones/Aritmetica.java
@@ -8,11 +8,22 @@ public class Aritmetica {
     int b;
     
     //Metodos
-    public void sumarNumero(){
+    public void sumarNumero(){ //el retorno es vacio
         //cuerpo de la clase, las variables aqui son locales. 
         int resultado = a + b;
         System.out.println("resultado = " + resultado);
         
     }
     
+    public int sumarConRetorno(){ //retorna un int
+        int resultado = a + b;
+        return resultado;
+    }
+    
+    public int sumaConArgumentos(int arg1, int arg2){
+        a = arg1;
+        b = arg2;
+        //return  a + b;
+        return sumarConRetorno();//solo se hacen con metodos de la misma clase No RECOMENDABLE
+    }
 }
